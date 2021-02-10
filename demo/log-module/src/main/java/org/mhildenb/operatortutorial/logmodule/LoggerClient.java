6cd1a212c33bc5d3bef13d01e172f472088680ba
Marc Hildenbrand <mwhildenbrand@gmail.com>
2021-02-10 13:27:06 +0000

    Remove unused import

--- demo/log-module/src/main/java/org/mhildenb/operatortutorial/logmodule/LoggerClient.java
+++ demo/log-module/src/main/java/org/mhildenb/operatortutorial/logmodule/LoggerClient.java
@@ -3,7 +3,6 @@ package org.mhildenb.operatortutorial.logmodule;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
