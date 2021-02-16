7ec2e84699411dc1d8f6f9cc01af1f2c4737f3db
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-02-16 12:25:05 +0000

    Remove unused include

--- demo/demo-operator/src/main/java/org/mhildenb/operatortutorial/demooperator/AppOpsController.java
+++ demo/demo-operator/src/main/java/org/mhildenb/operatortutorial/demooperator/AppOpsController.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
